0c5ecb91ed185652ee6a05b643e9f50f49e2640e
hiepquach <hieplp710@gmail.com>
2019-07-31 22:01:55 +0700

    - bulk edit status devices

---
+++ app/Http/Controllers/Admin/BulkEditStatusController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use Carbon\Carbon;
+use Illuminate\Routing\Controller;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Input;
+use Illuminate\Support\Facades\Request;
+
+class BulkEditStatusController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    public function bulkEditStatusDevice() {
+        return view('vendor/backpack/base/bulk_edit_status');
+    }
+
+    public function doDeleteLocation(Request $request) {
+        $time = Input::get('time');
+        $date = Carbon::createFromFormat('d/m/Y', $time);
+        $current = Carbon::now();
+        $diff = $date->diffInMonths($current);
+        $user = Auth::user();
+        if (!$user->hasRole('Admin')){
+            return response()->json(['status' => false, 'error' => "Bạn không có quyền thực hiện hành động này"]);
+        }
+        if ($diff < 2) {
+            return response()->json(['status' => false, 'error' => "Trước tháng hiện tại 2 tháng hoặc hơn"]);
+        }
+        $date->setTime(23, 59, 59);
+        $dateStr = $date->format('Y-m-d H:i:s');
+        $statement = "delete from device_locations where created_at <= '$dateStr';";
+        $deleted = DB::delete($statement);
+        return response()->json(['status' => true, 'error' => false]);
+    }
+}
